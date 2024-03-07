edd0621a195b6b33150e970af94f2c6eb29dbb32
mogrtou <1449666456@qq.com>
2024-03-07 20:36:00 +0800

    port from 80 change to 443 note: Http default port is 80       Https default port is 443

--- HttpTest.py
+++ HttpTest.py
@@ -3,36 +3,15 @@ import ssl
 
 
 def connect_server(address, port):
-    # tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM),
-    #                                  ca_certs="cert.pem", cert_reqs=ssl.CERT_REQUIRED
-    # certificate = ssl.get_server_certificate((address, port))
-    # print(certificate)
-    # context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    # context.set_ciphers('DEFAULT@SECLEVEL=2')
-    server_address = (address, port)
-
-    context = ssl.create_default_context()
-    tcp_socket = context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM),
-                                     server_hostname=server_address[0])
-    # tcp_socket = ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
-    tcp_socket.settimeout(500)
 
     server_addr = (address, port)
+    context = ssl.create_default_context()
+    tcp_socket = context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM),
+                                     server_hostname=server_addr[0])
     tcp_socket.connect(server_addr)
 
     return tcp_socket
 
-
-"User-Agent:Mozilla/5.0(Macintosh;Intel Mac 0sX10.15;rv:123.0)Gecko/2010010"
-"Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/we"
-"Accept-Language:en-US,en;g=0.5Accept-Encoding:gzip，deflate，br"
-"Referer: https://www.google.com/"
-"Connection:keep-alive"
-"Cookie: bid=quF_BCePijI; ap_v=0,6.0;__yadk_uid=TjpLQt5H6hkn6GvbRwvPFlQeDoHfewFQ"
-"Upgrade-Insecure-Requests:1"
-"Sec-Fetch-Dest:document"
-"Sec-Fetch-Mode:navigate"
-"Sec-Fetch-Site:cross-site"
 
 
 def data_content():
@@ -71,19 +50,10 @@ def data_content():
     return request_data
 
 
-# # 3. 发送数据
-# send_data = input("请输入要发送的数据：")
-# tcp_socket.send(send_data.encode("gbk"))
-#
-# # 4. 关闭套接字
-# tcp_socket.close()
-
 def main():
-    https = connect_server("www.zhihu.com", 80)  # 这里的端口怎么获取
+    https = connect_server("movie.douban.com", 443)  # 这里的端口怎么获取
 
     send_data = data_content()
-
-    print(send_data)
 
     https.send(send_data.encode())
 
